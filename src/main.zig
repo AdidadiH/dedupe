a9b408a905dfa5dadfc038fc4f5a909168d55a12
AdiFPV99 <hackler@MOVTOP.com.movtop.h>
2026-06-20 12:22:42 +0200

    added file reader
    prints the file out to test if working (only temporary)

--- src/main.zig
+++ src/main.zig
@@ -15,11 +15,32 @@ pub fn main(init: std.process.Init) !void {
     }
 
     //check file existens
-    var found = true;
-    std.Io.Dir.cwd().access(init.io, args[1], .{}) catch |e| switch (e) {
-        error.FileNotFound => found = false,
-        else => return e,
-    };
-    if (!found)
-        std.debug.print("\n\x1b[0;31;1mERROR: Invalid path!\n\nUsage:\n       dedupe path/to/file\x1b[0m\n\n", .{});
+    {
+        var found = true;
+        std.Io.Dir.cwd().access(init.io, args[1], .{}) catch |e| switch (e) {
+            error.FileNotFound => found = false,
+            else => return,
+        };
+        if (!found) {
+            std.debug.print("\n\x1b[0;31;1mERROR: Invalid path!\n\nUsage:\n       dedupe path/to/file\x1b[0m\n\n", .{});
+            return;
+        }
+    }
+    const srcPath = args[1];
+
+    //read in source file
+    const srcFile = try std.Io.Dir.cwd().openFile(init.io, srcPath, .{});
+    defer srcFile.close(init.io);
+
+    var src_file_buffer: [4096]u8 = undefined;
+    var src_reader = srcFile.reader(init.io, &src_file_buffer);
+
+    {
+        //NOTE: temporary, only for testing that it works
+        var line_no: usize = 0;
+        while (try src_reader.interface.takeDelimiter('\n')) |line| {
+            line_no += 1;
+            std.debug.print("{d}--{s}\n", .{ line_no, line });
+        }
+    }
 }
